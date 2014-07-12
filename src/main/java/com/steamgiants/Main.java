37caee0a68905e98d77e9164eb1fc295fa9958dd
Simon Hardijanto <metaluna4@hellokitty.com>
2014-07-12 08:07:02 +0000

    initial main class

---
+++ src/main/java/com/steamgiants/Main.java
@@ -0,0 +1,10 @@
+package com.steamgiants;
+
+public class Main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to Steam Giants!");
+    }
+}
